110b1269dbb3c3c193d2d463ae72230f4fc85e38
Tony.Liang <pillar0514@163.com>
2016-10-12 11:00:52 +0800

    update docs

--- src/view.js
+++ src/view.js
@@ -1,11 +1,79 @@
 var $assign = require('spore-kit-obj/src/assign');
 
 /**
+构建一个视图模块，用命名空间的方式完成事件绑定和数据绑定
+
+@module spore-kit-wapp/src/view
+@example
+
+// module.js
+var $assign = require('spore-kit-obj/src/assign');
+var $view = require('spore-kit-wapp/src/view');
+
+var module = function(options){
+	
+	var conf = $assign({
+		context : null,
+		ns : 'module'
+	}, options);
+
+	var model = {
+		data : [0, 1, 2]
+	};
+
+	var self = new $view({
+		context : conf.context,
+		ns : conf.ns,
+		model : model
+	});
+
+	self.action = function(){
+		model.data.push(model.data.length);
+		self.setData({
+			name : conf.ns
+		});
+	};
+
+	self.setData();
+	self.attach(
+		'action'
+	);
+
+	return model;
+
+};
+
+module.exports = module;
+
+// module.wxml
+
+// index.js
+var $module = require('./module');
+
+// 在 Page 中绑定视图对象
+Page({
+	data: {},
+	onLoad: function() {
+		// 对象被绑定在 this.list
+		// 数据被绑定在 this.data.list
+		$module({
+			context : this,
+			ns : 'list'
+		});
+	}
+});
+
+//index.wxml
 
 **/
 var View = function(options){
 	this.conf = $assign({
+		// 每个视图组件都需要传入 context 作为绑定视图的根节点
+		// context 可以是顶层的 Page , 也可以是一个实例化的 view
 		context : null,
+		// ns 参数为组件命名空间路径
+		// 数据将会被绑定到 context.data[ns]
+		// 事件函数 self.fn 将会被绑定到 context[ns + ':' + 'fn']
 		ns : '',
 		model : {}
 	}, options);
